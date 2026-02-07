97d9e717f01569893d869044e13caa9c9b89445a
toocomputer <2061889+toocomputer@users.noreply.github.com>
2026-02-07 18:16:15 -0500

    rename folder

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ async function pollDeploymentStatus(interval = 10000, timeout = 5 * 60 * 1000) {
 
 async function main() {
   try {
-    const results = await uploadFolder('./../../Pictures/OHPICS/_ready');
+    const results = await uploadFolder('./../../Pictures/Mixtape/_0');
     if (results === 'Upload folder empty') {
       console.log('Skipping upload, proceeding to trigger deployment...');
     } else {
